6584b048c491cdd85f261f658c1cda7ff61dc2f9
Lasse Hansen <lassebh9@hotmail.com>
2019-03-05 14:10:03 +0100

    Started working on front page UI

--- BlockChain and UI/src/js/app.js
+++ BlockChain and UI/src/js/app.js
@@ -3,22 +3,8 @@ App = {
   contracts: {},
 
   init: async function() {
-    // Load pets.
-    $.getJSON('../pets.json', function(data) {
-      var petsRow = $('#petsRow');
-      var petTemplate = $('#petTemplate');
+    // front page.
 
-      for (i = 0; i < data.length; i ++) {
-        petTemplate.find('.panel-title').text(data[i].name);
-        petTemplate.find('img').attr('src', data[i].picture);
-        petTemplate.find('.pet-breed').text(data[i].breed);
-        petTemplate.find('.pet-age').text(data[i].age);
-        petTemplate.find('.pet-location').text(data[i].location);
-        petTemplate.find('.btn-adopt').attr('data-id', data[i].id);
-
-        petsRow.append(petTemplate.html());
-      }
-    });
 
     return await App.initWeb3();
   },
@@ -39,27 +25,12 @@ App = {
     return App.bindEvents();
   },
 
-  bindEvents: function() {
-    $(document).on('click', '.btn-adopt', App.handleAdopt);
-  },
-
-  markAdopted: function(adopters, account) {
-    /*
-     * Replace me...
-     */
-  },
-
-  handleAdopt: function(event) {
-    event.preventDefault();
-
-    var petId = parseInt($(event.target).data('id'));
-
-    /*
-     * Replace me...
-     */
-  }
-
 };
+
+function imageFunction(att1, att2) {
+  console.log(att1 + " : " +att2);
+  window.location.assign("car.html?testsdcx123")
+}
 
 $(function() {
   $(window).load(function() {
